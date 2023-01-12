6bace55dd1979bc275e17799da8bfe2ec27b33cc
idogidog <liuxinlin6@163.com>
2023-01-12 15:08:07 +0800

    i

---
+++ src/main/java/com/fwwb/msd/msd1/Msd1Application.java
@@ -0,0 +1,13 @@
+package com.fwwb.msd.msd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fwwb/msd/msd1/Msd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwwb.msd.msd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
